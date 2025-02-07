c53b45f6eb56a3dab00c21422a3fea769d6f7048
vikram-vikky2002 <vikram.nitpy@gmail.com>
2025-02-07 16:16:57 +0530

    arrays and list

---
+++ Day6/QuickKart/QuickKartBusinessLayer/Seller.cs
@@ -0,0 +1,37 @@
+﻿namespace QuickKartBusinessLayer
+{
+    public class Seller
+    {
+        string sellerId;
+        static int sellerCount;
+        string sellerName;
+
+        public string SellerId { get { return sellerId; } set { sellerId = value; } }
+        public string[] SellerLocations { get; set; }
+        public string SellerName { get { return sellerName; } set { sellerName = value; } }
+
+        static Seller()
+        {
+            sellerCount = 0;
+        }
+
+        public Seller(int maxSize)
+        {
+            SellerId = "" + ++sellerCount;
+            SellerLocations = new string[maxSize];
+        }
+
+        public bool AddLocation(params string[] sellerLocations)
+        {
+            if(SellerLocations.Length > sellerLocations.Length)
+            {
+                SellerLocations = sellerLocations;
+
+                return true;
+            }
+
+            return false;
+            
+        }
+    }
+}
